283594da891f1abfcf16bb3e90f6fb27fb264617
kosh202 <raphael.s.k@hotmail.com>
2022-05-19 01:28:25 -0300

    avl tudo cert
    falta adicionar a bst e fazer a media da altura

--- EP2/EP2/main.c
+++ EP2/EP2/main.c
@@ -1,326 +1,226 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 typedef struct no {
-    int valor;
-    struct no* esquerdo, * direito;
-    short altura;
+  int valor;
+  struct no *esquerdo, *direito;
+  short altura;
 } No;
 
-
-#pragma region AVL
 /*
-    FunÁ„o que cria um novo nÛ
-    x -> valor a ser inserido no nÛ
-    Retorna: o endereÁo do nÛ criado
+    Fun√ß√£o que cria um novo n√≥
+    x -> valor a ser inserido no n√≥
+    Retorna: o endere√ßo do n√≥ criado
 */
-No* novoNo(int x) {
-    No* novo = malloc(sizeof(No));
+No *novoNo(int x) {
+  No *novo = malloc(sizeof(No));
 
-    if (novo) {
-        novo->valor = x;
-        novo->esquerdo = NULL;
-        novo->direito = NULL;
-        novo->altura = 0;
-    }
-    else
-        printf("\nERRO ao alocar nÛ em novoNo!\n");
-    return novo;
+  if (novo) {
+    novo->valor = x;
+    novo->esquerdo = NULL;
+    novo->direito = NULL;
+    novo->altura = 0;
+  } else
+    printf("\nERRO ao alocar n√≥ em novoNo!\n");
+  return novo;
 }
 
 /*
     Retorna o maior dentre dois valores
-    a, b -> altura de dois nÛs da ·rvore
+    a, b -> altura de dois n√≥s da √°rvore
 */
 short maior(short a, short b) { return (a > b) ? a : b; }
 
-//  Retorna a altura de um nÛ ou -1 caso ele seja null
-short alturaDoNo(No* no) {
-    if (no == NULL)
-        return -1;
-    else
-        return no->altura;
+//  Retorna a altura de um n√≥ ou -1 caso ele seja null
+short alturaDoNo(No *no) {
+  if (no == NULL)
+    return -1;
+  else
+    return no->altura;
 }
 
-//   Calcula e retorna o fator de balanceamento de um nÛ
-short fatorDeBalanceamento(No* no) {
-    if (no)
-        return (alturaDoNo(no->esquerdo) - alturaDoNo(no->direito));
-    else
-        return 0;
-}
-
-// --------- ROTA«’ES ---------------------------
-
-// funÁ„o para a rotaÁ„o ‡ esquerda
-No* rotacaoEsquerda(No* r) {
-    No* y, * f;
-
-    y = r->direito;
-    f = y->esquerdo;
-
-    y->esquerdo = r;
-    r->direito = f;
-
-    r->altura = maior(alturaDoNo(r->esquerdo), alturaDoNo(r->direito)) + 1;
-    y->altura = maior(alturaDoNo(y->esquerdo), alturaDoNo(y->direito)) + 1;
-
-    return y;
-}
-
-// funÁ„o para a rotaÁ„o ‡ direita
-No* rotacaoDireita(No* r) {
-    No* y, * f;
-
-    y = r->esquerdo;
-    f = y->direito;
-
-    y->direito = r;
-    r->esquerdo = f;
-
-    r->altura = maior(alturaDoNo(r->esquerdo), alturaDoNo(r->direito)) + 1;
-    y->altura = maior(alturaDoNo(y->esquerdo), alturaDoNo(y->direito)) + 1;
-
-    return y;
-}
-
-No* rotacaoEsquerdaDireita(No* r) {
-    r->esquerdo = rotacaoEsquerda(r->esquerdo);
-    return rotacaoDireita(r);
-}
-
-No* rotacaoDireitaEsquerda(No* r) {
-    r->direito = rotacaoDireita(r->direito);
-    return rotacaoEsquerda(r);
-}
-
-/*
-    FunÁ„o para realizar o balanceamento da ·rvore apÛs uma inserÁ„o ou remoÁ„o
-    Recebe o nÛ que est· desbalanceado e retorna a nova raiz apÛs o
-   balanceamento
-*/
-No* balancear(No* raiz) {
-    short fb = fatorDeBalanceamento(raiz);
-
-    // RotaÁ„o ‡ esquerda
-    if (fb < -1 && fatorDeBalanceamento(raiz->direito) <= 0)
-        raiz = rotacaoEsquerda(raiz);
-
-    // RotaÁ„o ‡ direita
-    else if (fb > 1 && fatorDeBalanceamento(raiz->esquerdo) >= 0)
-        raiz = rotacaoDireita(raiz);
-
-    // RotaÁ„o dupla ‡ esquerda
-    else if (fb > 1 && fatorDeBalanceamento(raiz->esquerdo) < 0)
-        raiz = rotacaoEsquerdaDireita(raiz);
-
-    // RotaÁ„o dupla ‡ direita
-    else if (fb < -1 && fatorDeBalanceamento(raiz->direito) > 0)
-        raiz = rotacaoDireitaEsquerda(raiz);
-
-    return raiz;
-}
-
-/*
-    Insere um novo nÛ na ·rvore
-    raiz -> raiz da ·rvore
-    x -> valor a ser inserido
-    Retorno: endereÁo do novo nÛ ou nova raiz apÛs o balanceamento
-*/
-No* inserir(No* raiz, int x) {
-    if (raiz == NULL) // ·rvore vazia
-        return novoNo(x);
-    else { // inserÁ„o ser· ‡ esquerda ou ‡ direita
-        if (x < raiz->valor)
-            raiz->esquerdo = inserir(raiz->esquerdo, x);
-        else if (x > raiz->valor)
-            raiz->direito = inserir(raiz->direito, x);
-        else
-            printf("\nInsercao nao realizada!\nO elemento %d a existe!\n", x);
-    }
-
-    // Recalcula a altura de todos os nÛs entre a raiz e o novo nÛ inserido
-    raiz->altura = maior(alturaDoNo(raiz->esquerdo), alturaDoNo(raiz->direito)) + 1;
-
-    // verifica a necessidade de rebalancear a ·rvore
-    raiz = balancear(raiz);
-
-    return raiz;
-}
-
-/*
-    FunÁ„o para remover um nÛ da ¡rvore bin·ria balanceada
-    Pode ser necess·rio rebalancear a ·rvore e a raiz pode ser alterada
-    por isso precisamos retornar a raiz
-*/
-No* remover(No* raiz, int chave) {
-    if (raiz == NULL) {
-        printf("Valor nao encontrado!\n");
-        return NULL;
-    }
-    else { // procura o nÛ a remover
-        if (raiz->valor == chave) {
-            // remove nÛs folhas (nÛs sem filhos)
-            if (raiz->esquerdo == NULL && raiz->direito == NULL) {
-                free(raiz);
-                printf("Elemento folha removido: %d !\n", chave);
-                return NULL;
-            }
-            else {
-                // remover nÛs que possuem 2 filhos
-                if (raiz->esquerdo != NULL && raiz->direito != NULL) {
-                    No* aux = raiz->esquerdo;
-                    while (aux->direito != NULL)
-                        aux = aux->direito;
-                    raiz->valor = aux->valor;
-                    aux->valor = chave;
-                    printf("Elemento trocado: %d !\n", chave);
-                    raiz->esquerdo = remover(raiz->esquerdo, chave);
-                    return raiz;
-                }
-                else {
-                    // remover nÛs que possuem apenas 1 filho
-                    No* aux;
-                    if (raiz->esquerdo != NULL)
-                        aux = raiz->esquerdo;
-                    else
-                        aux = raiz->direito;
-                    free(raiz);
-                    printf("Elemento com 1 filho removido: %d !\n", chave);
-                    return aux;
-                }
-            }
-        }
-        else {
-            if (chave < raiz->valor)
-                raiz->esquerdo = remover(raiz->esquerdo, chave);
-            else
-                raiz->direito = remover(raiz->direito, chave);
-        }
-
-        // Recalcula a altura de todos os nÛs entre a raiz e o novo nÛ inserido
-        raiz->altura =
-            maior(alturaDoNo(raiz->esquerdo), alturaDoNo(raiz->direito)) + 1;
-
-        // verifica a necessidade de rebalancear a ·rvore
-        raiz = balancear(raiz);
-
-        return raiz;
-    }
-}
-
-void imprimir(No* raiz, int nivel) {
-    int i;
-    if (raiz) {
-        imprimir(raiz->direito, nivel + 1);
-        printf("\n\n");
-
-        for (i = 0; i < nivel; i++)
-            printf("\t");
-
-        printf("%d", raiz->valor);
-        imprimir(raiz->esquerdo, nivel + 1);
-    }
-}
-
-No* novaAmostra(int elementos) // quantidade de elemntos
-{
-    // srand(time(NULL));
-    No* raiz = novoNo((rand() % 49) + 1);
-
-    for (int i = 0; i < elementos - 1; i++) {
-        raiz = inserir(raiz, (rand() % 49) + 1);
-    }
-    return raiz;
-}
-#pragma endregion
-
-#pragma region BST
-void bstIdentPrint(No* raiz, int espacos)
-{//vai ir para o ultimo da direita e imprime depois volta e imprime e vai ate o ultimo da esquerda e imprime
-    if (raiz != NULL)
-    {
-        bstIdentPrint(raiz->direito, espacos + 1);
-        for (int i = 0; i < espacos; i++)
-        {
-            printf("\t");//tabs
-        }
-        printf("%d\n", raiz->valor);
-        bstIdentPrint(raiz->esquerdo, espacos + 1);
-
-    }    
-}
-
-void adicionarBst(No* raiz, int x) {
-    if (raiz == NULL) // ·rvore vazia
-        return novoNo(x);
-    else { // inserÁ„o ser· ‡ esquerda ou ‡ direita 
-        if (x > raiz->valor)
-        {
-            if (raiz->direito == NULL)//verificar se a direita for nulo adicionar l·
-            {
-                raiz->direito = novoNo(x);
-                return;
-            }
-            adicionarBst(raiz->direito, x);
-        }
-        else if (x < raiz->valor)
-        {
-            if (raiz->esquerdo == NULL)
-                //verificar se a esquerda for nulo adicionar l·
-            {
-                raiz->esquerdo = novoNo(x);
-                return;
-            }
-            adicionarBst(raiz->esquerdo, x);
-        }
-        
-    }
-}
-
-No* BSTAmostra(int elementos) // quantidade de elemntos
-    {
-        No* raiz = novoNo(rand() % 50);
-
-        for (int i = 0; i < elementos; i++) {
-            inserir(raiz, rand() % 50);
-        }
-        return raiz;
-    }
-#pragma endregion
-
-
-int main(void) {
-    int res =0, qtdAmostras = 0, qtdElemento = 0;
-
-    do {
-
-        printf("\nDigite 1 para nova simulaÁ„o\nDigite 2 para sair\n");
-        scanf_s("%d", &res); // resposta
-
-        switch (res) {
-        case 1:
-            printf("Digite a quantidade de amostras: ");
-            scanf_s("%d", &qtdAmostras);
-            printf("Digite a quantidade de elementos para cada amostra: ");
-            scanf_s("%d", &qtdElemento);
-            for (int i = 0; i < qtdAmostras; i++)
-            {
-                printf("\nAVL amostra %d\n", i+1);
-                imprimir(novaAmostra(qtdElemento), 0);
-
-                printf("\nBST amostra %d\n  ", i + 1);
-                bstIdentPrint(BSTAmostra(qtdElemento), 0);
-            }
-
-            break;
-        case 2:
-            break;
-        }
-
-    } while (res != 2);
-
+//   Calcula e retorna o fator de balanceamento de um n√≥
+short fatorDeBalanceamento(No *no) {
+  if (no)
+    return (alturaDoNo(no->esquerdo) - alturaDoNo(no->direito));
+  else
     return 0;
 }
 
+// --------- ROTA√á√ïES ---------------------------
+
+// fun√ß√£o para a rota√ß√£o √† esquerda
+No *rotacaoEsquerda(No *r) {
+  No *y, *f;
+
+  y = r->direito;
+  f = y->esquerdo;
+
+  y->esquerdo = r;
+  r->direito = f;
+
+  r->altura = maior(alturaDoNo(r->esquerdo), alturaDoNo(r->direito)) + 1;
+  y->altura = maior(alturaDoNo(y->esquerdo), alturaDoNo(y->direito)) + 1;
+
+  return y;
+}
+
+// fun√ß√£o para a rota√ß√£o √† direita
+No *rotacaoDireita(No *r) {
+  No *y, *f;
+
+  y = r->esquerdo;
+  f = y->direito;
+
+  y->direito = r;
+  r->esquerdo = f;
+
+  r->altura = maior(alturaDoNo(r->esquerdo), alturaDoNo(r->direito)) + 1;
+  y->altura = maior(alturaDoNo(y->esquerdo), alturaDoNo(y->direito)) + 1;
+
+  return y;
+}
+
+No *rotacaoEsquerdaDireita(No *r) {
+  r->esquerdo = rotacaoEsquerda(r->esquerdo);
+  return rotacaoDireita(r);
+}
+
+No *rotacaoDireitaEsquerda(No *r) {
+  r->direito = rotacaoDireita(r->direito);
+  return rotacaoEsquerda(r);
+}
+
+/*
+    Fun√ß√£o para realizar o balanceamento da √°rvore ap√≥s uma inser√ß√£o ou remo√ß√£o
+    Recebe o n√≥ que est√° desbalanceado e retorna a nova raiz ap√≥s o
+   balanceamento
+*/
+No *balancear(No *raiz) {
+  short fb = fatorDeBalanceamento(raiz);
+
+  // Rota√ß√£o √† esquerda
+  if (fb < -1 && fatorDeBalanceamento(raiz->direito) <= 0)
+    raiz = rotacaoEsquerda(raiz);
+
+  // Rota√ß√£o √† direita
+  else if (fb > 1 && fatorDeBalanceamento(raiz->esquerdo) >= 0)
+    raiz = rotacaoDireita(raiz);
+
+  // Rota√ß√£o dupla √† esquerda
+  else if (fb > 1 && fatorDeBalanceamento(raiz->esquerdo) < 0)
+    raiz = rotacaoEsquerdaDireita(raiz);
+
+  // Rota√ß√£o dupla √† direita
+  else if (fb < -1 && fatorDeBalanceamento(raiz->direito) > 0)
+    raiz = rotacaoDireitaEsquerda(raiz);
+
+  return raiz;
+}
+
+/*
+    Insere um novo n√≥ na √°rvore
+    raiz -> raiz da √°rvore
+    x -> valor a ser inserido
+    Retorno: endere√ßo do novo n√≥ ou nova raiz ap√≥s o balanceamento
+*/
+No *inserir(No *raiz, int x, int *repetido) {
+  if (raiz == NULL) // √°rvore vazia
+    return novoNo(x);
+  else { // inser√ß√£o ser√° √† esquerda ou √† direita
+    if (x < raiz->valor)
+      raiz->esquerdo = inserir(raiz->esquerdo, x, repetido);
+    else if (x > raiz->valor)
+      raiz->direito = inserir(raiz->direito, x, repetido);
+    else
+      printf("\nInsercao nao realizada!\nO elemento %d a existe!\n", x);
+      repetido = 1;
+  }
+
+  // Recalcula a altura de todos os n√≥s entre a raiz e o novo n√≥ inserido
+  raiz->altura =
+      maior(alturaDoNo(raiz->esquerdo), alturaDoNo(raiz->direito)) + 1;
+
+  // verifica a necessidade de rebalancear a √°rvore
+  raiz = balancear(raiz);
+
+  return raiz;
+}
+
+void imprimir(No *raiz, int nivel) {
+  int i;
+  if (raiz) {
+    imprimir(raiz->direito, nivel + 1);
+    printf("\n\n");
+
+    for (i = 0; i < nivel; i++)
+      printf("\t");
+
+    printf("%d", raiz->valor);
+    imprimir(raiz->esquerdo, nivel + 1);
+  }
+}
+
+No *novaAmostra(int elementos, int *array) // quantidade de elemntos
+{
+    No* raiz = novoNo(array[0]);
+    int repetido = 0;
+  for (int i = 1; i < elementos; i++) {
+    raiz = inserir(raiz, array[i], repetido);
+    if(repetido == 1)
+    {
+        repetido = 0;
+        do
+        {
+            raiz = inserir(raiz, rand()%50, repetido);
+        }while(repetido != 1);
+        
+    }
+  }
+  return raiz;
+}
+
+void criarNumros(int *array, int qtdElemento) {
+  for (int i = 0; i < qtdElemento; i++) {
+    array[i] = rand() % 50;
+  }
+}
+
+//BST
+
+
+int main() {
+
+  int opcao, valor;
+  No *raiz = NULL;
+
+  do {
+    printf("\n\n\t0 - Sair\n\t1 - Criar arvore\n");
+    scanf("%d", &opcao);
+
+    switch (opcao) {
+    case 0:
+      printf("Saindo!!!");
+      break;
+    case 1: {
+        raiz = NULL;
+      printf("quantidade de elementos: ");
+      int elementos;
+
+      scanf("%d", &elementos);
+      int array[elementos];
+
+      criarNumros(array, elementos);
+      raiz = novaAmostra(elementos, array);
+      printf("AVL");
+      imprimir(raiz, 1);
+      break;
+    }
+    default:
+      printf("\nOcao invalida!!!\n");
+    }
+
+  } while (opcao != 0);
+
+  return 0;
+}
